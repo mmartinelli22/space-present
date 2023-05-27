1326e363e400838ebdc09fb5d52bd19ebc800b3b
Michael Martinelli <turtelboy123@gmail.com>
2023-05-27 14:56:57 -0600

    plswork

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit IT WERK <code>src/App.js</code> and save to reload.
+          Edit ITasds  WERK <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
